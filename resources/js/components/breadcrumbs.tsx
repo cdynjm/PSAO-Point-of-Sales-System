67309fff0542ec7024a15265da5f73c4c94543e3
Jemuel Cadayona <cdynjm@gmail.com>
2025-11-27 10:14:15 +0800

    updated and modified some changes

--- resources/js/components/breadcrumbs.tsx
+++ resources/js/components/breadcrumbs.tsx
@@ -3,6 +3,7 @@ import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
 import { Link } from '@inertiajs/react';
 import { HomeIcon } from 'lucide-react';
 import { Fragment } from 'react';
+import AppLogoIcon from './app-logo-icon';
 
 export function Breadcrumbs({ breadcrumbs }: { breadcrumbs: BreadcrumbItemType[] }) {
     return (
@@ -17,9 +18,19 @@ export function Breadcrumbs({ breadcrumbs }: { breadcrumbs: BreadcrumbItemType[]
                                     <BreadcrumbItem>
                                         {isLast ? (
                                             <BreadcrumbPage>
-                                                <div className="flex items-center gap-1">
+                                                <div className="hidden items-center gap-1 md:flex">
                                                     <HomeIcon className="mr-1 inline h-4 w-4" />
                                                     <span>{item.title}</span>
+                                                </div>
+                                                <div className="md:hidden">
+                                                    <div className="flex items-center gap-1">
+                                                        <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+                                                            <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                                                        </div>
+                                                        <div className="ml-1 grid flex-1 text-left text-xs">
+                                                            <span className="mb-0.5 truncate leading-tight font-light">Laravel</span>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </BreadcrumbPage>
                                         ) : (
